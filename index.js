e9e1dcd4f4c20c090fdf326a220ac1e1858597a3
hans334 <84727741+xhanwt@users.noreply.github.com>
2022-02-03 13:11:37 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/public/about.html");
-});
-
-app.get("/contact", (req, res) => {
-  res.sendFile(__dirname + "/public/contact.html");
-});
-
-app.use(express.static(__dirname + "/public"), (_, res, next) => {
-  res.status(404);
-  res.sendFile(__dirname + "/public/404.html");
-});
+app.use(
+  express.static(__dirname + "/public", { extensions: ["html"] }),
+  (_, res, next) => {
+    res.status(404);
+    res.sendFile(__dirname + "/public/404.html");
+  }
+);
 
 app.listen(8080);
