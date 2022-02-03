d41e45c281f49f1d127e544558d72734a1bad94c
hans334 <84727741+xhanwt@users.noreply.github.com>
2022-02-03 13:45:15 +0630

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const express = require("express");
 const app = express();
 const publicDirPath = path.join(__dirname, "../public");
 
-app.use(express.static(publicDirPath, { extensions: ["html"] }));
+var fs = require("fs");
+
+app.use(function (req, res, next) {
+  if (req.path.indexOf(".") === -1) {
+    var file = publicDirPath + req.path + ".html";
+    fs.exists(file, function (exists) {
+      if (exists) req.url += ".html";
+      next();
+    });
+  } else next();
+});
+app.use(express.static(publicDirPath));
 
 app.use(function (req, res) {
   res.status(404).sendFile(publicDirPath + "/404.html");
