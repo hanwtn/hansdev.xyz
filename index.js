2ed47100984a264840d01da5960833821bf7c730
hans334 <84727741+xhanwt@users.noreply.github.com>
2022-02-03 14:17:19 +0630

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ app.use(
   express.static(publicDirPath, { index: false, extensions: ["html"] })
 );
 
-app.use(function (req, res, next) {
-  res.status(404).sendFile(publicDirPath + "/404.html");
-});
-
 app.listen(8080, () => {
   console.log("Server is up.");
 });
